9a7309fd1dcd6359435c73fa80d2e8e64a22bb67
Zaqueu Alves <61361618+Zaqueu-exe@users.noreply.github.com>
2021-06-14 10:18:02 -0300

    Update Uri_1118.java

--- iniciante/Uri_1118.java
+++ iniciante/Uri_1118.java
@@ -7,15 +7,13 @@ public class Uri_1118 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         DecimalFormat df = new DecimalFormat("0.00");
-        float[] x = new float[25];
-        float[] x1 = new float[25];
+        float x, x1;
         boolean nota = false, novo;
-        int cont = 0, opc;
+        int opc;
         while(!nota){
-            x[cont] = testarNota(scan);
-            x1[cont] = testarNota(scan);
-            System.out.println("media = " + df.format((x[cont] + x1[cont])/2));
-            cont++;
+            x = testarNota(scan);
+            x1 = testarNota(scan);
+            System.out.println("media = " + df.format((x + x1)/2));
 
             novo = false;
             while(!novo){
