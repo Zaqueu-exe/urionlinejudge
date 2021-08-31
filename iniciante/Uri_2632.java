7cb3cc51fd5b5da6f6a04a5ec0d831a8fca91048
Zaqueu Alves <61361618+Zaqueu-exe@users.noreply.github.com>
2021-08-31 02:03:32 -0300

    Update Uri_2632.java

--- iniciante/Uri_2632.java
+++ iniciante/Uri_2632.java
@@ -30,119 +30,48 @@ public class Uri_2632 {
     }
 
     public static int resultado(String magia, int n) {
-        int alcance = 0, dano = 0;
-        switch (magia) {
-            case "fire": {
-                dano = 200;
-                switch (n) {
-                    case 1: {
-                        alcance = 20;
-                    }
-                        break;
-                    case 2: {
-                        alcance = 30;
-                    }
-                        break;
-                    case 3: {
-                        alcance = 50;
-                    }
-                        break;
-                }
+        int alcance = 0, dano = 0; 
+        if (magia.equals("fire")) {
+            dano = 200;
+            if (n == 1) {
+                alcance = 20;
+            } else if (n == 2) {
+                alcance = 30;
+            } else {
+                alcance = 50;
             }
-                break;
-            case "water": {
-                dano = 300;
-                switch (n) {
-                    case 1: {
-                        alcance = 10;
-                    }
-                        break;
-                    case 2: {
-                        alcance = 25;
-                    }
-                        break;
-                    case 3: {
-                        alcance = 40;
-                    }
-                        break;
-                }
+        } else if (magia.equals("water")) {
+            dano = 300;
+            if (n == 1) {
+                alcance = 10;
+            } else if (n == 2) {
+                alcance = 25;
+            } else {
+                alcance = 40;
             }
-                break;
-            case "earth": {
-                dano = 400;
-                switch (n) {
-                    case 1: {
-                        alcance = 25;
-                    }
-                        break;
-                    case 2: {
-                        alcance = 55;
-                    }
-                        break;
-                    case 3: {
-                        alcance = 70;
-                    }
-                        break;
-                }
+        } else if (magia.equals("earth")) {
+            dano = 400;
+            if (n == 1) {
+                alcance = 25;
+            } else if (n == 2) {
+                alcance = 55;
+            } else {
+                alcance = 70;
             }
-                break;
-            case "air": {
-                dano = 100;
-                switch (n) {
-                    case 1: {
-                        alcance = 18;
-                    }
-                        break;
-                    case 2: {
-                        alcance = 38;
-                    }
-                        break;
-                    case 3: {
-                        alcance = 60;
-                    }
-                        break;
-                }
+        } else {
+            dano = 100;
+            if (n == 1) {
+                alcance = 18;
+            } else if (n == 2) {
+                alcance = 38;
+            } else {
+                alcance = 60;
             }
-                break;
         }
         return dano + alcance;
     }
 
     public static double comparaD(int x0, int y0, int cx, int cy, int w, int h) {
-       /* double distancia = 9999999;
-        double[] aux = new double[6];
-        aux[0] = Math.sqrt(Math.pow((x0 - cx) + w, 2) + Math.pow((y0 - cy) + h, 2));
-        aux[1] = Math.sqrt(Math.pow((x0 - cx) + w, 2) + Math.pow((y0 - cy), 2));
-        aux[2] = Math.sqrt(Math.pow((x0 - cx), 2) + Math.pow((y0 - cy) + h, 2));
-        aux[3] = Math.sqrt(Math.pow((x0 - cx), 2) + Math.pow((y0 - cy), 2));
-
-        if (x0 <= cx && (x0 + w) >= cx) {
-            aux[4] = (y0 + h) - cy;
-        } else if (y0 <= cy && (y0 + h) >= cy) {
-            aux[4] = (x0 + w) - cx;
-        } else if ((x0 >= cx) && (y0 <= cy) && (y0 + h) >= cy) {
-            aux[4] = x0 - cx;
-        }else if(y0 >= cy && x0 <= cx && (x0 + w) >= cx){
-            aux[4] = y0 - cy;
-        }
-        if (x0 <= cx && (x0 + w) >= cx) {
-            aux[5] = y0 - cy;
-        } else if (y0 <= cy && (y0 + h) >= cy) {
-            aux[5] = x0 - cx;
-        } else if (x0 >= cx && y0 <= cy && (y0 + h) >= cy) {
-            aux[5] = (x0+w) - cx;
-        }else if(y0 >= cy && x0 <= cx && (x0 + w) >= cx){
-            aux[5] = (y0+h) - cy;
-        }
-        aux[4] = Math.abs(aux[4]); 
-        aux[5] = Math.abs(aux[5]); 
-
-        for (int i = 0; i < 6; i++) {
-            System.out.println(aux[i] + "==========");
-            if (aux[i] < distancia) {
-                distancia = aux[i];
-            }
-        }*/
         double distancia = 0;
         double x = cx;
         double y = cy;
